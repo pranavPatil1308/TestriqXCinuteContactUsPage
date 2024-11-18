dc5b1052fc9b5dde43f6a7d9710978de894accdb
SantoshKak <skakade@meetsoci.com>
2024-11-18 10:02:52 +0530

    updated test script

--- src/test/java/Demo/Test/test1.java
+++ src/test/java/Demo/Test/test1.java
@@ -6,7 +6,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
@@ -19,15 +18,15 @@ public class test1 {
 	public static WebDriverWait wait;
 	@BeforeMethod
 	public void setup() {
-		ChromeOptions options =  new ChromeOptions();
-		options.addArguments("--headless");
-		driver=new ChromeDriver(options) ;
+		//ChromeOptions options =  new ChromeOptions();
+		//options.addArguments("--headless");
+		driver=new ChromeDriver() ;
 	driver.manage().window().maximize();
 	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 	driver.get("https://cinutedigital.com/contact-us/");
 	}
 	@Test (priority =1)
-	public void demoTesttwo(){
+	public void demoTesttwo() throws InterruptedException{
 		
 		WebElement name = driver.findElement(By.id("name"));
 		name.sendKeys("Test");
@@ -45,8 +44,8 @@ public class test1 {
 		 WebElement element1 = wait1.until(ExpectedConditions.elementToBeClickable(By.xpath("(//button[text()='Next'])[1]")));
 		    element1.click();
 		    
-		  WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-		  WebElement element = wait.until(ExpectedConditions.presenceOfElementLocated(By.id("education")));
+		    Thread.sleep(2000);
+		    WebElement element = driver.findElement(By.id("education"));
 		  Select w = new Select (element);
 		 w.selectByVisibleText("Graduate");
 		
